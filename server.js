8c73caf4fc9d084863ca56f988985af1d09e3aae
Orkun <ork.74@hotmail.com>
2026-01-28 14:17:50 +0300

    DB connection fixed?

--- server.js
+++ server.js
@@ -32,7 +32,12 @@ const dbase = mysql.createConnection({
     host: process.env.DB_HOST,      
     user: process.env.DB_USER,      
     password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME    
+    database: process.env.DB_NAME,
+    port: 10001,
+    ssl: {
+        // Bu ayar sunucunun sertifikasını otomatik kabul eder
+        rejectUnauthorized: false 
+    }
 });
 
 dbase.connect(err => {
